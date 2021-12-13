67b759022696c61df96ec60d780d3cbc5ea3416d
LENOVO <LENOVO@LAPTOP-2BOEPIAK>
2021-12-13 10:49:33 +0530

    workspace 2 commit 1

--- src/main/java/GitmainTest/GitTest1.java
+++ src/main/java/GitmainTest/GitTest1.java
@@ -1,5 +1,12 @@
 package GitmainTest;
 
+import org.testng.annotations.Test;
+
 public class GitTest1 {
 
+	@Test
+	public void gitTestp() {
+		System.out.println(" workspace 2 made changes");
+		
+	}
 }
